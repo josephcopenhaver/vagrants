f52bd20c39a26d6db083f3232c353b877230341f
Joe Copenhaver <joseph.copenhaver@rewardstyle.com>
2019-01-13 22:24:15 -0600

    install rmate with rcode alias

--- environments/dev/provision.sh
+++ environments/dev/provision.sh
@@ -296,6 +296,7 @@ echo install basic linux packages
         htop \
         netcat \
         git \
+        tmux \
         tmate \
         build-essential
 )
@@ -371,6 +372,32 @@ echo install base python runtime
         python-openssl \
         \
         build-essential
+)
+
+echo install rcode as an alias for rmate
+(
+    set -exo pipefail
+
+    if ( command -v rcode ); then
+        echo "rcode already installed"
+        rcode --version || true
+        exit 0
+    fi
+
+    rm -rf /tmp/rmate.dl.tmp
+    curl -fsSL https://raw.github.com/aurora/rmate/master/rmate | install -m 755 /dev/stdin /tmp/rmate.dl.tmp
+
+    # rmate has a non-zero exit code when calling --help and --version
+    # so need to account for that
+    #
+    # test that the binary downloaded correctly
+    ( /tmp/rmate.dl.tmp --version 2>&1 || true ) | grep -qE '[0-9]+\.[0-9]+\.[0-9]+'
+
+    # install the binary
+    mv /tmp/rmate.dl.tmp /usr/local/bin/rmate
+    ln -s rmate /usr/local/bin/rcode
+
+    ensure_ver_logged rcode 'rcode --version' true
 )
 
 echo configure python version manager
